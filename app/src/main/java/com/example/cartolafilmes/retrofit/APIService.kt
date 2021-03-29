8aad92c3d937bfd73c66bbb8cd701d058d0ce84c
luana-barbosa <luana_barbosa93@hotmail.com>
2021-03-29 12:18:58 -0300

    Configuracao de API com Retrofit

---
+++ app/src/main/java/com/example/cartolafilmes/retrofit/APIService.kt
@@ -0,0 +1,19 @@
+package com.example.cartolafilmes.retrofit
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object APIService {
+    var retrofit : Retrofit? = null
+        get(){
+
+            return if (field == null){
+                Retrofit.Builder()
+                    .baseUrl("https://api.themoviedb.org/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            }else{
+                field
+            }
+        }
+    }
