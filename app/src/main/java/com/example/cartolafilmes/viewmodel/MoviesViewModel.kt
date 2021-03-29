46f4d5b87fa672c1b26808de663e392df53d6301
luana-barbosa <luana_barbosa93@hotmail.com>
2021-03-29 19:21:58 -0300

    Adicionando ViewModel e atualizando bibliotecas no gradle

--- app/src/main/java/com/example/cartolafilmes/viewmodel/MoviesViewModel.kt
+++ app/src/main/java/com/example/cartolafilmes/viewmodel/MoviesViewModel.kt
@@ -1,2 +1,27 @@
 package com.example.cartolafilmes.viewmodel
 
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cartolafilmes.model.Movies
+import com.example.cartolafilmes.repository.MoviesRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MoviesViewModel : ViewModel(){
+
+    private var moviesRepository : MoviesRepository = MoviesRepository()
+
+    private val moviesList : MutableLiveData<List<Movies>> = MutableLiveData()
+
+    fun getMovies() {
+        viewModelScope.launch (Dispatchers.IO){
+            moviesList.postValue(moviesRepository.getMovies())
+        }
+    }
+    fun observeMovies(
+        lifecycleOwner: LifecycleOwner,
+        action: (List<Movies>) -> Unit
+    ) = moviesList.observe(lifecycleOwner, { action(it)})
+}
