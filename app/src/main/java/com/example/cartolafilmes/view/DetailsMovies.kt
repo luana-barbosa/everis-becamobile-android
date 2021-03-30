63e652d1fdc944ce638308cc5f180fffc7fdd767
luana-barbosa <luana_barbosa93@hotmail.com>
2021-03-30 20:47:56 -0300

    Configurando a pagina Detalhes

--- app/src/main/java/com/example/cartolafilmes/view/DetailsMovies.kt
+++ app/src/main/java/com/example/cartolafilmes/view/DetailsMovies.kt
@@ -8,21 +8,41 @@ import com.example.cartolafilmes.R
 import com.example.cartolafilmes.model.Movie
 import com.example.cartolafilmes.response.Movies
 import com.example.cartolafilmes.utils.getJsonExtra
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_details_movies.*
+import kotlinx.android.synthetic.main.activity_details_movies.title_movie
+import kotlinx.android.synthetic.main.movie_item.*
+import kotlinx.android.synthetic.main.movie_item.poster_movie as poster_movie1
 
 class DetailsMovies : AppCompatActivity() {
 
-    lateinit var movies : Movies
+    private lateinit var movies : Movies;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details_movies)
 
-        val movies: Movie? = intent.getJsonExtra(Movie::class.java)
-        val title = findViewById<TextView>(R.id.title_movie)
-        val overview = findViewById<TextView>(R.id.overview_movie)
-        val releaseDate = findViewById<TextView>(R.id.date_movie)
-        val backdropPath = findViewById<ImageView>(R.id.backdrop_movie)
-        val posterPath = findViewById<ImageView>(R.id.poster_movie)
+        movies = intent.getSerializableExtra("movie_extra") as Movies
+        title_movie.text = movies.title
+        overview_movie.text = movies.overview
+        date_movie.text = movies.releaseDate
+        Picasso.get().load(movies.backdropPath).into(backdrop_movie)
+        Picasso.get().load(movies.posterPath).into(poster_movie)
         //val voteAverage = findViewById<TextView>(R.id.title_movie)
        // val popularity = findViewById<TextView>(R.id.title_movie)
+
+    movies?.let{
+        title_movie.text = it.title
+        overview_movie.text = it.overview
+        date_movie.text = it.releaseDate
+        if(it.posterPath?.isNotEmpty() == true){
+            Picasso.get().load(movies.posterPath).into(poster_movie)
+             if(!it.backdropPath?.isEmpty()!!){
+                 Picasso.get().load(movies.backdropPath).into(backdrop_movie)
+            }else{
+                Picasso.get().load(R.mipmap.ic_launcher).into(poster_movie)
+            }
+        }
     }
+}
 }
