ddb8b9f6167e527d7b4c362c5fe160e791886e7e
luana-barbosa <luana_barbosa93@hotmail.com>
2021-03-30 10:04:58 -0300

    Ajustes de ligacao do Adapter com a Tela1

--- app/src/main/java/com/example/cartolafilmes/view/MoviesAdapter.kt
+++ app/src/main/java/com/example/cartolafilmes/view/MoviesAdapter.kt
@@ -4,13 +4,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.cartolafilmes.R
 import com.example.cartolafilmes.response.Movies
-import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MoviesAdapter (var movies: List<Movies>) : RecyclerView.Adapter<MoviesAdapter.ViewHolder>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.movie_item,
             parent,false)
    }
 
